cedf3fcd3d42cf6e37142e462a8724aca6dbdf6d
Hserfx <90092793+Hserfx@users.noreply.github.com>
2022-03-31 08:57:07 +0200

    final idk if it works

--- main.py
+++ main.py
@@ -3,41 +3,58 @@ import time
 
 WIDTH = 800
 HEIGHT = 600
+drawn = False
+
+def prime(n):
+    for i in range(2, n//2):
+        if n%i == 0:
+            return False
+    return True
+
 
 def change_direction(dx, dy):
     if dx == 1:
+        print('gora')
         return (0, -1)
     elif dx == -1:
+        print('dol')
         return (0, 1)
     elif dy == 1:
-        return (-1, 0)
+        print('prawo')
+        return (1, 0)
     elif dy == -1:
+        print('lewo')
         return (-1, 0)
 
 def draw(display):
-    c = 1
-    n = 1
+    global drawn
+    c = 2
+    n = 2
     dx = 1
     dy = 0
     x = WIDTH//2
     y = HEIGHT//2
-    surface = FONT.render(str(0), True, (255, 0, 0))
+    surface = FONT.render(str(2), True, (255, 0, 0))
     display.blit(surface, (x, y))
 
-    for i in range(1, 30):
-        x += dx*30
-        y += dy*30
+    if drawn:
+        return
+    for i in range(3, 10000):
+        x += dx*5
+        y += dy*5
         print((dx, dy))
-        surface = FONT.render(str(i), True, (255, 0, 0))
-        display.blit(surface, (x, y))
+        if prime(i):
+            rect = pg.Rect((x, y), (5, 5))
+            pg.draw.rect(display, (255, 0, 0), rect)
         c -= 1
         if c == 0:
-            print('chuj')
             dx, dy = change_direction(dx, dy)
             c = n
             n += 1
-        pg.display.flip() 
-        time.sleep(1)
+     
+        pg.display.flip()
+    
+    drawn = True
 
                 
 
@@ -62,14 +79,14 @@ def events():
 if __name__ == '__main__':
     pg.init()
     pg.font.init()
-    display = pg.display.set_mode(size=(WIDTH, HEIGHT))
+    display = pg.display.set_mode((WIDTH, HEIGHT))
     CLOCK = pg.time.Clock()
  
-    FONT = pg.font.SysFont('Comic Sans ms', 16)
+    FONT = pg.font.SysFont('Comic Sans ms', 3)
 
 
     while True:
-        CLOCK.tick(1)
+        CLOCK.tick(144)
         draw(display)
         events()
 
